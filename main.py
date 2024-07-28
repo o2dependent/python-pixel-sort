8386941a9b3c8ebb2b78a19efc9947b342861df2
Ethan Olsen <131eolsen@gmail.com>
2024-07-27 22:30:00 -0700

    feat: better sort

--- main.py
+++ main.py
@@ -16,15 +16,25 @@ def get_flesh_tone_mask(
     return mask
 
 
-def sort_pixels(image: cv2.typing.MatLike) -> cv2.typing.MatLike:
+def sort_pixels(
+    image: cv2.typing.MatLike, mask: cv2.typing.MatLike
+) -> cv2.typing.MatLike:
     width, height, _ = image.shape
     for i in range(width):
-        row = list()
+        arr = list()
+        index_arr = list()
         for j in range(height):
-            row.append(image[i, j])
-        row = sorted(row, key=lambda v: v[0])
-        for j in range(height):
-            image[i, j] = row[j]
+            if mask[i, j] == 255:
+                arr.append(image[i, j])
+                index_arr.append(j)
+            #     arr.append((j, image[i, j]))
+
+        arr = sorted(arr, key=lambda v: v[2])
+
+        for j in range(len(arr)):
+            color = arr[j]
+            index = index_arr[j]
+            image[i, index] = color
     return image
 
 
@@ -41,9 +51,9 @@ def main():
         flesh_tone_mask = get_flesh_tone_mask(image)
         flesh_tone_mask_inv = cv2.bitwise_not(flesh_tone_mask)
         copied_image = image.copy()
-        sorted_copy = sort_pixels(copied_image)
-        result_bg = cv2.bitwise_and(sorted_copy, sorted_copy, mask=flesh_tone_mask)
-        result_fg = cv2.bitwise_and(image, image, mask=flesh_tone_mask_inv)
+        sorted_copy = sort_pixels(copied_image, flesh_tone_mask_inv)
+        result_bg = cv2.bitwise_and(sorted_copy, sorted_copy, mask=flesh_tone_mask_inv)
+        result_fg = cv2.bitwise_and(image, image, mask=flesh_tone_mask)
         result = cv2.add(result_bg, result_fg)
 
         cv2.imshow("OG", image)
@@ -54,8 +64,8 @@ def main():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    except ValueError:
-        print(ValueError)
+    except ValueError as e:
+        print(f"{e}")
 
 
 main()
