967633f2a40414d4e71b2440e93488dbb1a202e7
Ethan Olsen <131eolsen@gmail.com>
2024-07-28 17:45:33 -0700

    chore: rename result, add lum var, remove unneeded image copy, add copy image to sort_pixel to prevent mutation

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_flesh_tone_mask(
 
 
 def get_luminance_mask(
-    image: cv2.typing.MatLike, threshold_value
+    image: cv2.typing.MatLike, threshold_value=128
 ) -> cv2.typing.MatLike:
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
@@ -25,8 +25,9 @@ def get_luminance_mask(
 
 
 def sort_pixels(
-    image: cv2.typing.MatLike, mask: cv2.typing.MatLike
+    _image: cv2.typing.MatLike, mask: cv2.typing.MatLike
 ) -> cv2.typing.MatLike:
+    image = _image.copy()
     width, height, _ = image.shape
     for i in range(width):
         arr = list()
@@ -58,19 +59,20 @@ def main():
         image = cv2.imread(path)
         # flesh_tone_mask = get_flesh_tone_mask(image)
         # flesh_tone_mask_inv = cv2.bitwise_not(flesh_tone_mask)
-
-        luminance_mask = get_luminance_mask(image, 48)
+        lum_thresh = 68
+        luminance_mask = get_luminance_mask(image, lum_thresh)
         luminance_mask_inv = cv2.bitwise_not(luminance_mask, 0)
 
-        copied_image = image.copy()
-        sorted_copy = sort_pixels(copied_image, luminance_mask)
-        # result_bg = cv2.bitwise_and(sorted_copy, sorted_copy, mask=luminance_mask_inv)
+        sorted_result = sort_pixels(image, luminance_mask)
+        inverted_sorted_result = sort_pixels(image, luminance_mask_inv)
+        # result_bg = cv2.bitwise_and(sorted_result, sorted_result, mask=luminance_mask_inv)
         # result_fg = cv2.bitwise_and(image, image, mask=luminance_mask)
         # result = cv2.add(result_bg, result_fg)
 
         cv2.imshow("OG", image)
         cv2.imshow("Luminance mask", luminance_mask)
-        cv2.imshow("Result", sorted_copy)
+        cv2.imshow("Sorted result", sorted_result)
+        cv2.imshow("Inverted sorted result", inverted_sorted_result)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
